c3e92572ca0ef0f2bb4dff0b0385c94c62f541af
Akanksha8761 <akanksha8761@gmail.com>
2026-04-02 12:27:51 +0530

    Update module1.py

--- python-practice-journey/day-16/module1.py
+++ python-practice-journey/day-16/module1.py
@@ -1,9 +1,26 @@
-# script1.py
+# My first module module1.py
 def greet(name):
-    return f"Hello, {name}!"
-# ... use it ...
+    """Prints a greeting message to the console."""
+    return f"Hello, {name}! Welcome to Palampur! This is my first module"
 
-# script2.py
-def greet(name):  # Copy-paste the SAME function!
-    return f"Hello, {name}!"
-# ... use it again ...
+def calculate(num1, num2):
+    """Returns multiple calculations of two numbers."""
+    sum_result = num1 + num2
+    sub = num1 - num2
+    multi = num1 * num2
+    divide = num1 / num2
+    return sum_result, sub, multi, divide
+
+# This block of code will ONLY execute if module1.py is run directly.
+# It will NOT execute if module1.py is imported into another script.
+if __name__ == "__main__":
+    print("------- module1.py is being run directly -------")
+    print("This section is for testing or demonstrating the functions in module1.py.\n")
+    print(greet("John"))
+    print("------ Calling calculate() from within ---------")
+    add, subtract, multiply, division = calculate(30, 10)
+    print(f"The addition is {add}")
+    print(f"The subtract is {subtract}")
+    print(f"The multiplication is {multiply}")
+    print(f"The division is {division}")  # This will print a float value
+    print("------ Ending module1.py ---------")
