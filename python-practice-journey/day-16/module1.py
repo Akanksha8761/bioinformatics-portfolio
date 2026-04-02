9f0e31747e56680cd05858d45695187aecc573d1
Akanksha8761 <akanksha8761@gmail.com>
2026-04-02 12:26:52 +0530

    Create module1.py

---
+++ python-practice-journey/day-16/module1.py
@@ -0,0 +1,9 @@
+# script1.py
+def greet(name):
+    return f"Hello, {name}!"
+# ... use it ...
+
+# script2.py
+def greet(name):  # Copy-paste the SAME function!
+    return f"Hello, {name}!"
+# ... use it again ...
